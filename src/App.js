cfc44d9368ec68e885b41d201d5b6afb62f97498
Marco Salas <marco.ssalas.85@gmail.com>
2022-04-21 17:35:35 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
+import React from 'react';
 import './App.css';
+import Testimonio from './componentes/Testimonio';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  render()  {
+    return (
+      <div className="App">
+      <div className='contenedor-principal'>
+        
+          <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+          <Testimonio
+            nombre='Shawn Wang'
+            imagen='shawn'
+            pais='Singapur'
+            cargo='Ingeniero de Software'
+            empresa='Amazon'
+            testimonio='Da miedo cambiar de carrera. Solo gane experiencia'/>
+          <Testimonio 
+            nombre='Emma Bostian'
+            pais='Suecia'
+            imagen='emma'
+            cargo='Ingeniera de Software'
+            empresa='Spotify'
+            testimonio='Siempre he tenido problemas para aprender JavaSvript'/>
+          <Testimonio
+            nombre='Sarah'
+            pais='Nigeria'
+            imagen='sarah'
+            cargo='Ingeniera de Software'
+            empresa='ChatDesk'
+            testimonio='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevo a mis conocimientos de programación de un nivel principiante total a un nivel muy seguro.'/>  
+         
+      </div>  
     </div>
-  );
+    );
+  }
 }
+
 
 export default App;
